30a9bf7677a3f76c9dbff5a055ddf68e39548b3c
Mallika <mallikak2709@gmail.com>
2016-04-04 16:22:07 -0700

    The class heirarchy for decisiontree

---
+++ ServerML/src/main/java/edu/sjsu/smartsecure/dataAccess/CleansedDataHandler.java
@@ -0,0 +1,19 @@
+package edu.sjsu.smartsecure.dataAccess;
+
+/**
+ * Created by mallika on 4/4/16.
+ */
+public class CleansedDataHandler {
+    int hh;
+    int newo;
+    int yquy;
+    public void pushDataAppAndLocationData(int blah) {
+
+    }
+
+    public void pushOccupation() {
+
+    }
+
+
+}
---
+++ ServerML/src/main/java/edu/sjsu/smartsecure/dataAccess/MongoFactory.java
@@ -0,0 +1,7 @@
+package edu.sjsu.smartsecure.dataAccess;
+
+/**
+ * Created by mallika on 4/4/16.
+ */
+public class MongoFactory {
+}
