5983ef777a644086c3d37483807ca4116c85801a
Apoorva <g.apoorvareddy@gmail.com>
2016-04-19 18:33:26 -0700

    Fixed a bug for Data Usage and tested

--- ServerML/src/main/java/edu/sjsu/smartsecure/controller/DecisionTreeController.java
+++ ServerML/src/main/java/edu/sjsu/smartsecure/controller/DecisionTreeController.java
@@ -24,8 +24,8 @@ public class DecisionTreeController {
         //TODO: can move the training data part to a separate thread to train data every 24hrs
         try {
             EvalDataCleanseService evalDataCleanseService = new EvalDataCleanseService();
-//            evalDataCleanseService.cleanTrainData();
-//            evalDataCleanseService.trainData();
+            evalDataCleanseService.cleanTrainData();
+            evalDataCleanseService.TrainData();
         }catch(Exception e){
             e.printStackTrace();
         }
